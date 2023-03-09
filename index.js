dabf1f99af3e879356b4ace8e62047648073e497
Katarzyna Łada <96846727+KasiaLada@users.noreply.github.com>
2023-03-09 14:21:31 +0100

    zadanka

---
+++ index.js
@@ -0,0 +1,63 @@
+const name = "Kasia";
+const cat = "Pnelopa";
+
+console.log(`czesc, jestem ${name}, a to jest ${cat}, mój kot`);
+
+const catAge = 16;
+
+console.log("Pnelopa ma już " + catAge + "lat " + name + "!");
+
+const msg = "abgkfddggjhhj";
+console.log(msg.toUpperCase());
+
+// const text1 = "powieksz mnie";
+// console.log(text1.toUpperCase());
+
+// const text2 = "ZAPISZ MNIE MAŁYMI LITERAMI";
+// console.log(text2.toLowerCase())
+
+// const text3 = "$#%#^ wytnij te dziwne znaki na początku!";
+// console.log(text3.slice(6));
+
+// const text4 = 'sprawdź, czy zawieram słowo "czy"';
+// console.log(text4.includes("czy"));
+
+// const text5 =
+//     'wyLoguj w konsoli tylko literę "L", która znajduje się w wyrazie "Wyloguj"';
+// console.log(text5.charAt(2))
+
+// const text6 = "pies zamień każde słowo pies, na słowo kot pies";
+// console.log(text6.replaceAll("pies", "kot"));
+
+// const text7 = "podziel, ten, string, od, przecinków";
+// console.log(text7.split(","));
+
+// const text1 = "powiększ mnie!";
+// const text2 = "ZAPISZ MNIE MAŁYMI LITERAMI";
+// const text3 = "$#%#^ wytnij te dziwne znaki na początku!";
+// const text4 = 'sprawdź, czy zawieram słowo "czy"';
+// const text5 =
+// 	'wyLoguj w konsoli tylko literę "L", która znajduje się w wyrazie "Wyloguj"';
+// const text6 = "pies zamień każde słowo pies, na słowo kot pies";
+// const text7 = "podziel, ten, string, od, przecinków";
+
+// console.log(text1.toUpperCase());
+// console.log(text2.toLowerCase());
+// console.log(text3.slice(6));
+// console.log(text4.includes("czy"));
+// console.log(text5.charAt(2));
+// console.log(text6.replaceAll("pies", "kot"));
+// console.log(text7.split(","));
+
+// console.log(10 === "10");
+
+// const pass = "dgdgd!fgff";
+
+// if (pass.length > 10 && pass.includes("!")) {
+// 	// console.log(pass.includes("!"));
+// 	console.log("masz reweacyjne hasło");
+// } else if (pass.length > 10) {
+// 	console.log("masz dobre hasło");
+// } else {
+// 	console.log("masz za krótkie hasło");
+// }
